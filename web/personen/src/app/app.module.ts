a32f1531bb55adc218a4f8358963db0c9f7e419e
OngenaePieter <pieterbracke@hotmail.com>
2020-01-20 09:51:36 +0100

    Inital commit inital commit, basis layout

---
+++ web/personen/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+})
+export class AppModule { }
